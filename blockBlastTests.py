fae3fcf406946c6025c532e43a0132a9e368b3cd
Logan Costello <logancostello2022@gmail.com>
2024-12-30 13:29:06 -0800

    tests for checking and playing moves

--- blockBlastTests.py
+++ blockBlastTests.py
@@ -1,5 +1,5 @@
 import unittest
-from blockBlastSolver import checkClear, createGame
+from blockBlastSolver import checkClear, createGame, canPlayPiece, playPiece
 
 class Tests(unittest.TestCase):
     def test_checkClear_1(self):
@@ -28,3 +28,51 @@ class Tests(unittest.TestCase):
         expected = [[0, 0], [0, 0], [0, 0], [0, 0]]
 
         self.assertEqual(game, expected)
+
+    def test_playPiece_1(self):
+        game = createGame(2, 2)
+        playPiece(game, 0, 0, [[0, 0]])
+        expected = [[1, 0], [0, 0]]
+
+        self.assertEqual(game, expected)
+
+    def test_playPiece_2(self):
+        game = createGame(2, 2)
+        playPiece(game, 0, 0, [[0, 0], [1, 0], [0, 1], [1, 1]])
+        expected = [[1, 1], [1, 1]]
+
+        self.assertEqual(game, expected)
+
+    def test_canPlayPiece_1(self):
+        game = createGame(2, 2)
+        piece = [[0, 0]]
+
+        self.assertTrue(canPlayPiece(game, 0, 0, piece))
+        self.assertTrue(canPlayPiece(game, 1, 1, piece))
+        self.assertFalse(canPlayPiece(game, 2, 2, piece))
+
+    def test_canPlayPiece_2(self):
+        game = createGame(3, 3)
+        playPiece(game, 0, 0, [[0, 0]])
+        piece = [[0, 0]]
+
+        self.assertFalse(canPlayPiece(game, 0, 0, piece))
+   
+    def test_canPlayPiece_3(self):
+        game = createGame(3, 3)
+        playPiece(game, 0, 0, [[1, 1]])
+        
+        self.assertTrue(canPlayPiece(game, 0, 0, [[0, 0], [1, 0], [2, 0], [0, 1], [0, 2]]))
+        self.assertFalse(canPlayPiece(game, 0, 0, [[0, 0], [1, 0], [0, 1], [1, 1]]))
+
+
+
+
+
+
+
+
+
+
+
+
