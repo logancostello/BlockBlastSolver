6eeda1e1c5f40e63653d91cc1cecedabc6964b5b
Logan Costello <logancostello2022@gmail.com>
2024-12-31 22:31:35 -0800

    func to find best moves for multiple pieces in order

--- blockBlastSolver.py
+++ blockBlastSolver.py
@@ -63,5 +63,75 @@ def playPiece(game, desiredRow, desiredCol, piece):
         col = desiredCol + colOffset
         game[row][col] = 1
 
+# Function to score the current state of the game
+# Used for searching and evaluating future positions
+def scoreCurrentState(game):
+    # Each open space increases the score by 1
+    # Each open neighbor of an open space increases the score by 1
+    # This way, large open spaces are scored higher
+    score = 0
+    for row in range(len(game)):
+        for col in range(len(game[0])):
+            if game[row][col] == 0:
+                score += 1
+                if row - 1 > 0 and game[row - 1][col] == 0:
+                    score += 1
+                if row + 1 < len(game) and game[row + 1][col] == 0:
+                    score += 1
+                if col - 1 > 0 and game[row][col - 1] == 0:
+                    score += 1
+                if col + 1 < len(game[0]) and game[row][col + 1] == 0:
+                    score += 1
+    
+    return score
+
+# Find all places a piece can be played
+def getAllPossibleMoves(game, piece):
+    possibleMoves = []
+    for row in range(len(game)):
+        for col in range(len(game[0])):
+            if canPlayPiece(game, row, col, piece):
+                possibleMoves.append([row, col])
+    
+    return possibleMoves
+
+# Given a game and a piece, find the best spot to play it
+def findBestMove(game, piece):
+    possibleMoves = getAllPossibleMoves(game, piece)
+    scores = []
+
+    for row, col in possibleMoves:
+        gameCopy = game
+        playPiece(gameCopy, row, col, piece)
+        checkClear(gameCopy)
+        score = scoreCurrentState(game)
+        scores.append(score)
+
+    if scores:
+        maxScore = max(scores)
+        return [possibleMoves[scores.index(maxScore)], maxScore]
+    else:
+        return [None, float("-inf")]
+
+# Given a list of pieces, find the best way to play them the given order
+# Return the positions and the final score
+def findBestMovesInOrder(game, pieces):
+    if len(pieces) == 1:
+        pos, score = findBestMove(game, pieces[0])
+        return [[pos], score]
+    else:
+        maxScore = float('-inf')
+        bestMoves = None
+        piece = pieces.pop(0)
+        possibleMoves = getAllPossibleMoves(game, piece)
+        for row, col in possibleMoves:
+            gameCopy = game
+            playPiece(gameCopy, row, col, piece)
+            positions, score = findBestMovesInOrder(gameCopy, pieces)
+            if score > maxScore:
+                maxScore = score
+                bestMoves = [[row, col]] + positions
+        return [bestMoves, maxScore]
+
 
 
