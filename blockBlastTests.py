0dba7fb8d79659725e50a280482bcb5c74c6c8ac
Logan Costello <logancostello2022@gmail.com>
2024-12-31 22:31:51 -0800

    tests for findBestMovesInOrder func

--- blockBlastTests.py
+++ blockBlastTests.py
@@ -1,5 +1,5 @@
 import unittest
-from blockBlastSolver import checkClear, createGame, canPlayPiece, playPiece
+from blockBlastSolver import checkClear, createGame, canPlayPiece, playPiece, scoreCurrentState, findBestMove, findBestMovesInOrder
 
 class Tests(unittest.TestCase):
     def test_checkClear_1(self):
@@ -65,14 +65,80 @@ class Tests(unittest.TestCase):
         self.assertTrue(canPlayPiece(game, 0, 0, [[0, 0], [1, 0], [2, 0], [0, 1], [0, 2]]))
         self.assertFalse(canPlayPiece(game, 0, 0, [[0, 0], [1, 0], [0, 1], [1, 1]]))
 
+    def test_scoreCurrentState_1(self):
+        game1 = createGame(3, 3)
+        game2 = createGame(3, 3)
+        game3 = createGame(3, 3)
+        playPiece(game2, 0, 0, [[0, 0]])
+        playPiece(game3, 1, 1, [[0, 0]])
 
+        self.assertTrue(scoreCurrentState(game1) > scoreCurrentState(game2))
+        self.assertTrue(scoreCurrentState(game2) > scoreCurrentState(game3))
 
+    def test_scoreCurrentState_2(self):
+        game1 = createGame(2, 2)
+        game2 = createGame(2, 2)
+        piece = [[0, 0], [1, 0]]
+        playPiece(game1, 0, 0, piece)
+        playPiece(game2, 0, 1, piece)
 
+        self.assertEqual(scoreCurrentState(game1), scoreCurrentState(game2))
 
+    def test_scoreCurrentState_3(self):
+        game1 = createGame(2, 2)
+        game2 = createGame(2, 2)
+        playPiece(game1, 0, 0, [[0, 0], [1, 0]])
+        playPiece(game2, 0, 0, [[0, 0], [1, 1]])
 
+        self.assertTrue(scoreCurrentState(game1) > scoreCurrentState(game2))
 
+    def test_findBestMove_1(self):
+        game = createGame(2, 2)
+        piece = [[0, 0], [1, 0], [0, 1]]
+        pos, score = findBestMove(game, piece)
 
+        self.assertEqual(pos, [0, 0])
 
+    def test_findBestMove_2(self):
+        game = createGame(3, 3)
+        piece = [[1, 0], [0, 1], [1, 1], [1, 2]]
 
+        pos, score = findBestMove(game, piece)
+
+        self.assertEqual(pos, [1, 0])
+
+    def test_findBestMove_3(self):
+        game = createGame(3, 3)
+        playPiece(game, 0, 0, [[0, 0], [0, 1]])
+        piece = [[0, 0]]
+        pos, score = findBestMove(game, piece)
+
+        self.assertEqual(pos, [0, 2])
+
+    def test_findBestMove_4(self):
+        game = createGame(1, 1)
+        pos, score = findBestMove(game, [[0, 0], [1, 0]])
+
+        self.assertEqual(pos, None)
+        self.assertEqual(score, float('-inf'))
+
+    def test_findBestMovesInOrder_1(self):
+        game = createGame(3, 3)
+        piece1 = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        piece2 = [[0, 2], [1, 2], [2, 2], [2, 0], [2, 1]]
+        pieces = [piece1, piece2]
+
+        moves, score = findBestMovesInOrder(game, pieces)
+
+        self.assertEqual(moves, [[0, 0], [0, 0]])
+
+    def test_findBestMovesInOrder_2(self):
+        game = createGame(4, 4)
+        piece = [[0, 0]]
+        pieces = [piece, piece, piece, piece]
+
+        moves, score = findBestMovesInOrder(game, pieces)
+        
+        self.assertEqual(score, scoreCurrentState(game))
 
 
